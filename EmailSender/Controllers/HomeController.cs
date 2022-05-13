d1d3ac62f911a5d344d0c3238e45ebc5f9b86022
LukSt92 <stach.stachu@gmail.com>
2022-05-13 20:41:17 +0200

    Added email send function; made some changes to views.

--- EmailSender/Controllers/HomeController.cs
+++ EmailSender/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using EmailSender.Models.Domains;
+using EmailSender.Models.Repositories;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +13,61 @@ namespace EmailSender.Controllers
 {
     public class HomeController : Controller
     {
+
+        private MessageRepository _messageRepository = new MessageRepository();
         public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Message message)
+        {
+            var userId = User.Identity.GetUserId();
+            message.UserId = userId;
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _messageRepository.Add(message);
+                }
+                var senderEmail = new MailAddress("lukst92reportservice@gmail.com", message.Sender);
+                var receiverEmail = new MailAddress(message.Receiver, "Receiver");
+                var password = "";
+                var sub = message.Title;
+                var body = message.MessageContent;
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(senderEmail.Address, password)
+                };
+                using (var mess = new MailMessage(senderEmail, receiverEmail)
+                {
+                    Subject = sub,
+                    Body = body
+                })
+                {
+                    smtp.Send(mess);
+                }
+                return RedirectToAction("Sent");
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+
+        public ActionResult Sent()
         {
             return View();
         }
@@ -24,6 +83,11 @@ namespace EmailSender.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult SendEmail()
+        {
             return View();
         }
     }
---
+++ EmailSender/Models/Repositories/MessageRepository.cs
@@ -0,0 +1,21 @@
+﻿using EmailSender.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmailSender.Models.Repositories
+{
+    public class MessageRepository
+    {
+
+        public void Add(Message message)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Messages.Add(message);
+                context.SaveChanges();
+            }
+        }
+    }
+}
